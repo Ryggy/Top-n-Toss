f7e37400eee2ff211b7e925c7897cbbf8f828bcc
Liam W <1031438@student.sae.edu.au>
2024-11-18 14:37:11 +1000

    Bounce multiplier mostly works
    Each bounce adds an amount to total multiplier
    Have to implement a reset to multiplier

---
+++ Food Fling/Assets/Scripts/Multiplier/MultiplierManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplierManager : MonoBehaviour
+{
+
+    public float multTotal = 1;
+    public float multPerBounce = 1;
+
+}
